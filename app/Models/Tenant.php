6c7be7799e3cb4a1140fb6d8c0c38dd11d076d0e
AKM Elias <akmelias11@gmail.com>
2025-04-03 21:58:46 +0600

    Initial commit: Laravel 11 multi-tenant invoice management system setup

---
+++ app/Models/Tenant.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+
+class Tenant extends BaseTenant implements TenantWithDatabase
+{
+    use HasDatabase, HasDomains;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'phone',
+        'address',
+        'logo',
+        'settings',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'settings' => 'array',
+    ];
+
+    public static function getCustomColumns(): array
+    {
+        return [
+            'id',
+            'name',
+            'email',
+            'phone',
+            'address',
+            'logo',
+            'settings',
+        ];
+    }
+} 
